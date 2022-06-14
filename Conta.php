638f0b999fb58bfde547c7c4848eed9c598c37b4
x <karolina_bp@hotmail.com>
2022-06-14 14:46:08 -0300

    Retirando os else dos métodos
    - Definimos um return no if para pausar a execução do método depois que tudo dentro do bloco foi feito, com isso não preciso do else

--- Conta.php
+++ Conta.php
@@ -12,29 +12,29 @@ class Conta
         //this se refere a referência atual que chamou esse método
         if($valorASacar > $this->saldo){
             echo "Saldo indisponível";
-        } else{
-            $this->saldo -= $valorASacar;
+            return;
         }
+
+        $this->saldo -= $valorASacar;
     }
 
     public function depositar(float $valorADepositar): void{
         if($valorADepositar < 0){
             echo "Valor precisa ser positivo";
+            return;
         }
-        else{
-            $this->saldo += $valorADepositar;
-        }
+
+        $this->saldo += $valorADepositar;
     }
 
     public function transferir(float $valorATransferir, Conta $contaDestino) : void{
         if($valorATransferir > $this->saldo){
             echo "Saldo indisponível";
+            return;
         }
-        else{
-            $this->sacar($valorATransferir);
-            $contaDestino->depositar($valorATransferir);
-        }
-
+        
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
     }
 }
 
