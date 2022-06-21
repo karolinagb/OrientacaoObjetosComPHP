5a28ff9967b1e90d470d3a296ee001c31b98c5a7
x <karolina_bp@hotmail.com>
2022-06-21 16:02:25 -0300

    Definição uso método __toString()

--- teste-enderecos.php
+++ teste-enderecos.php
@@ -10,5 +10,9 @@ $segundoEndereco = new Endereco('Rio de Janeiro', 'Copacabana', 'Rua final', '81
 //Uso o método toString() para eu nao ter que imprimir assim:
 //echo $umEndereco->recuperarRua() . ", " . $umEndereco->recuperarNumero() . ", " ...
 //No método __toString() eu já defino essa formatação
+
+//Alternativa correta! Sempre que tentarmos representar um objeto como string 
+//(seja com echo, passando por parâmetro ou retornando) o método __toString é procurado,
+//e se a classe o tiver, seu retorno é utilizado.
 echo $umEndereco . PHP_EOL;
 echo $segundoEndereco . PHP_EOL;
